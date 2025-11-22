9ccb7205b34c68e3fd4d195e6e7503e70fd72016
Yaroslav Latypov <jarlat04@lapuas.xyz>
2025-11-22 23:30:53 +0100

    added addition

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 class Matrix{
     private:
         int rows, cols;
@@ -21,15 +20,47 @@ class Matrix{
             int i = r * cols + c;
             return vals[i];
         }
+
+        Matrix operator+(Matrix& secMatrix){
+            Matrix result(rows,cols);
+
+            if ( rows != secMatrix.rows || cols != secMatrix.cols) {
+                std::cout << "Matrices have different dimensions";    
+            } else {
+                for (int i = 0 ; i < rows*cols; i++) {
+                    result.vals[i] = vals[i] + secMatrix.vals[i];
+                }
+            }
+        return result;
+        }
         
 };
 
 
 int main(){
 
-    Matrix m(4,5);
-    m(1,2) = 5;
-    std::cout << "value: " << m(1,2) << "\n";
+    int dim = 1;
 
+    Matrix m(dim,dim);
+    Matrix n(dim,dim);
+    Matrix l(3,3);
+
+    m(0,0) = 5.5;
+    m(0,1) = 1.0;
+    m(1,0) = 5.1;
+    m(1,1) = 5.1;
+
+    n(0,0) = 6.1;
+    n(0,1) = 9.4;
+    n(1,0) = 2.0;
+    n(1,1) = 8.7;
+
+    Matrix sum = m+n;
+
+    for (int r = 0; r <= dim; r++) {
+        for (int c = 0; c <=dim; c++){
+            std::cout << sum(r, c) << "\n";
+        }
+    }
     return 0;
 }
