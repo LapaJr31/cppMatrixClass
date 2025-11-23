671e68746827f7a9f10facbb9f9f5fc9d52a9c4b
Yaroslav Latypov <jarlat04@lapuas.xyz>
2025-11-23 19:40:47 +0100

    added matrix on matrix multiplication

--- matrix.cpp
+++ matrix.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <random>
-
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_real_distribution<double> dis(0.1,10.0); 
+#include <stdexcept>
 
 class Matrix{
     private:
@@ -26,23 +22,23 @@ class Matrix{
             return vals[i];
         }
 
-        // Matrix operator()(std::string flag){
-        //     if(flag == "random"){
-        //         for (int i = 0; i < rows*cols; i++){
-        //             vals[i] = dis(gen);
-        //         }
-        //         return *this;
-        //     } else {
-        //         throw *this;
-        //     }
-        // }
+        int operator()(std::string input){
+            if (input == "size"){
+                return rows * cols;
+            } else if(input == "cols") {
+                return cols;
+            } else if(input == "rows") {
+                return rows;
+            } else {
+                throw std::runtime_error("Invalid input");
+            }
+        }
 
-        // addition
+          // addition
         Matrix operator+(Matrix& secMatrix){
             Matrix result(rows,cols);
-
             if ( rows != secMatrix.rows || cols != secMatrix.cols) {
-                std::cout << "Matrices have different dimensions";    
+                throw std::runtime_error("Matrices have different dimensions");
             } else {
                 for (int i = 0 ; i < rows*cols; i++) {
                     result.vals[i] = vals[i] + secMatrix.vals[i];
@@ -66,7 +62,7 @@ class Matrix{
         }
         
         // scalar multiplication
-        Matrix operator*=(double num){
+        Matrix operator*(double num){
             for (int i = 0; i < rows*cols; i++){
                 vals[i] *=  num;
             }
@@ -74,17 +70,24 @@ class Matrix{
         }
 
         // in progress matrix on matrix
-         Matrix operator*=(Matrix& secMatrix){
-            Matrix result(rows,cols);
-            std::cout << "Matrices have different dimensions";    
-            for (int i = 0 ; i < rows*cols; i++) {
-                result.vals[i] = vals[i] - secMatrix.vals[i];
-            }
-        return result;
-        }
-        
-};
+        Matrix operator*(Matrix& secMatrix){
+            Matrix result(rows,secMatrix.cols);
 
+            if (cols != secMatrix.rows){
+                throw std::runtime_error("Cols don't match the rows of second Matrix");
+            }
+            for (int i = 0 ; i < result.rows; i++) {
+                for (int j = 0 ; j < result.cols; j++) {
+                    double sum = 0.0;
+                    for(int k = 0; k < cols; k++){
+                        sum += (*this)(i,k) * secMatrix(k,j) ;
+                    }
+                    result(i,j) = sum;
+                }
+            }
+            return result;
+        }
+};
 
 int main(){
 
@@ -92,10 +95,20 @@ int main(){
 
     Matrix m(dim, dim);
     Matrix n(dim,dim);
-    Matrix l(3,3);
+
+    m(0,0) = 1.2;
+    m(0,1) = 8.5;
+    m(1,0) = 0.2;
+    m(1,1) = 4.8;
+
+    n(0,0) = 9.1;
+    n(0,1) = 6.0;
+    n(1,0) = 4.2;
+    n(1,1) = 7.7;
 
     Matrix sum = m+n;
     Matrix dif = m-n;
+    Matrix mult = m*n;
 
     std::cout << "Sum:\n";
     for (int r = 0; r < dim; r++) {
@@ -103,6 +116,7 @@ int main(){
             std::cout << sum(r, c) << "\n";
         }
     }
+    std::cout << "\n";
 
     std::cout << "Dif:\n";
     for (int r = 0; r < dim; r++) {
@@ -110,5 +124,15 @@ int main(){
             std::cout << dif(r, c) << "\n";
         }
     }
+    std::cout << "\n";
+
+    std::cout << "Multiplications:\n";
+    for (int r = 0; r < mult("rows"); r++) {
+        for (int c = 0; c < mult("cols"); c++){
+            std::cout << mult(r, c) << "\n";
+        }
+    }
+    std::cout << "\n";
+
     return 0;
 }
